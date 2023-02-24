d0ce01c5d6c78a209877eae98d749b6c5d31616a
Jack <jpedler96@outlook.com>
2023-02-25 09:52:20 +1100

    refactor: remove id from TestDataInserter data

--- develop/insert_test_data.py
+++ develop/insert_test_data.py
@@ -11,19 +11,30 @@ class TestDataInserter:
     def insert(cls):
         table_factory = TableFactory()
         email_request = table_factory.create("email_request")
+        email_type = table_factory.create("email_type")
         email_recipient = table_factory.create("email_recipient")
         config = Initialiser.create_config()
         engine = create_engine(config.database.connection_string)
 
-        sub_query = (
+        recipient_sub_query = (
             select(email_recipient.c.id)
             .where(email_recipient.c.email_address == bindparam("email_address"))
             .scalar_subquery()
         )
 
+        type_sub_query = (
+            select(email_type.c.id)
+            .where(email_type.c.name == bindparam("email_type"))
+            .scalar_subquery()
+        )
+
         with engine.connect() as conn:
             conn.execute(insert(email_recipient), cls.__create_email_recipient_data())
-            conn.execute(insert(email_request).values(email_recipient_id=sub_query), cls.__create_email_request_data())
+            conn.execute(
+                insert(email_request)
+                .values(email_recipient_id=recipient_sub_query, email_type_id=type_sub_query),
+                cls.__create_email_request_data()
+            )
             conn.commit()
 
     @classmethod
@@ -40,12 +51,12 @@ class TestDataInserter:
     @classmethod
     def __create_email_request_data(cls) -> list[dict]:
         return [
-            {"email_address": "jane@email.com", "email_type_id": 1},
-            {"email_address": "james@email.com", "email_type_id": 1},
-            {"email_address": "andrew@email.com", "email_type_id": 1},
-            {"email_address": "meredith@email.com", "email_type_id": 2},
-            {"email_address": "sam@email.com", "email_type_id": 2},
-            {"email_address": "charlie@email.com", "email_type_id": 2},
+            {"email_address": "jane@email.com", "email_type": "Welcome"},
+            {"email_address": "james@email.com", "email_type": "Welcome"},
+            {"email_address": "andrew@email.com", "email_type": "Welcome"},
+            {"email_address": "meredith@email.com", "email_type": "Invoice"},
+            {"email_address": "sam@email.com", "email_type": "Invoice"},
+            {"email_address": "charlie@email.com", "email_type": "Invoice"},
         ]
 
 
